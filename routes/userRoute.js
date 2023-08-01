cc0d1035714c79b47cd46f9058863881ed0535ed
Joseph <josephpeter489@gmail.com>
2023-08-01 10:02:48 +0100

    my fifth comiit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -44,7 +44,7 @@ route.delete('/deleteSchool/:id', deleteSchool)
 route.post('/teacherLink/:id', teacherLink)
 
 
-http://progresspal-8rxj.onrender.com/progressPal/newTeacher/64c8b9fed81e56d697e2eb93/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2NGM4YjlmZWQ4MWU1NmQ2OTdlMmViOTMiLCJpYXQiOjE2OTA4Nzc1MzEsImV4cCI6MTY5MDg3OTMzMX0.QzlueGbYFH1ngNnLYmw--1RDbWWfeiI8IYA3D4D9EQg
+// http://progresspal-8rxj.onrender.com/progressPal/newTeacher/64c8b9fed81e56d697e2eb93/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2NGM4YjlmZWQ4MWU1NmQ2OTdlMmViOTMiLCJpYXQiOjE2OTA4Nzc1MzEsImV4cCI6MTY5MDg3OTMzMX0.QzlueGbYFH1ngNnLYmw--1RDbWWfeiI8IYA3D4D9EQg
 
 // Route for Teachers Alone.
 // route.post('/newTeacher/:id/:token', upload.single('teacherImage'), newTeacher)
