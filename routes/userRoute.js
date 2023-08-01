16c8d6a05aea3aeda15a698f4d9c8ba81fd07db5
Joseph <josephpeter489@gmail.com>
2023-08-01 09:15:39 +0100

    my fourth comiit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -44,10 +44,11 @@ route.delete('/deleteSchool/:id', deleteSchool)
 route.post('/teacherLink/:id', teacherLink)
 
 
-
+http://progresspal-8rxj.onrender.com/progressPal/newTeacher/64c8b9fed81e56d697e2eb93/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2NGM4YjlmZWQ4MWU1NmQ2OTdlMmViOTMiLCJpYXQiOjE2OTA4Nzc1MzEsImV4cCI6MTY5MDg3OTMzMX0.QzlueGbYFH1ngNnLYmw--1RDbWWfeiI8IYA3D4D9EQg
 
 // Route for Teachers Alone.
-route.post('/newTeacher/:id/:token', upload.single('teacherImage'), newTeacher)
+// route.post('/newTeacher/:id/:token', upload.single('teacherImage'), newTeacher)
+route.post('/newTeacher/:id/:token', newTeacher)
 route.post('/loginTeacher', teacherLogin)
 route.post('/logoutTeacher/:id', signOutTeacher)
 route.put("/changePasswordTeacher/:id", changePasswordTeacher);
